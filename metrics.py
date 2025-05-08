11e2607766809f91e8a1c550430ccdc300f7c824
Marcelo Maza <mjmaza02@gmail.com>
2025-05-07 22:27:20 -0400

    WIP

--- metrics.py
+++ metrics.py
@@ -1,11 +1,7 @@
 import numpy as np
 
 def prep_im(im, index):
-    im = im.copy()
-    im[im!=index]=-1
-    im[im==index]=0
-    im+=1
-    return im.astype(np.uint8)
+    return im == index
 
 def classIoU(ground:np.array, pred:np.array, index):
     """
@@ -42,9 +38,14 @@ def classAccuracy(ground, pred, index):
     pred = prep_im(pred, index)
     ground = prep_im(ground, index)
 
-    true_vals = np.sum(ground==pred)
-    return true_vals/(ground.shape[0]*ground.shape[1])
+    true_vals = np.logical_not(np.logical_xor(pred, ground)).sum()
+    return true_vals/ground.size
 
+def accuracy(pred, ground, num_classes):
+    acc = np.array([])
+    for i in range(num_classes):
+      acc = np.append(acc, [classAccuracy(ground, pred, i)])
+    return acc
 def meanIoU(ground, pred, num_classes):
     iou = []
     for i in range(num_classes):
